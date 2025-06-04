5bf2561328302c016918f544eac3e471129db562
MaksimObozniy <mobozn@gmail.com>
2025-06-04 21:11:52 +0400

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from .models import Place
+from .models import Place, PlaceImage
 # Register your models here.
 
-admin.site.register(Place)
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+    fields = ['image', 'order'] 
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
--- places/models.py
+++ places/models.py
@@ -7,9 +7,24 @@ class Place(models.Model):
     description_long = models.TextField(verbose_name='Длинное описание')
     latitude = models.FloatField(verbose_name='Широта')
     longtitude = models.FloatField(verbose_name='Долгота')
-    
-    imgs = models.JSONField(verbose_name='Ссылки на фотографии', blank=True)
 
     def __str__(self):
         return self.title
-        
+
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Место',
+        related_name='images'
+    )
+    
+    image = models.ImageField(upload_to='places/', verbose_name="Картинка")
+    order = models.PositiveIntegerField(default=0, verbose_name='Порядок')
+    
+    class Meta:
+        ordering = ['order'] 
+    
+    def __str__(self):
+        return f"{self.order} {self.place.title}"
