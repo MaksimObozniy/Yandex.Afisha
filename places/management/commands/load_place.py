42a4c5da74c4be73e282b2b6060ac6dd15c779ed
MaksimObozniy <mobozn@gmail.com>
2025-06-05 22:26:01 +0400

    Add management command to import places from JSON

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,42 @@
+import requests
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+from places.models import Place, PlaceImage
+
+class Command(BaseCommand):
+    help = 'Загружает место из JSON по ссылке'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_url', type=str, help='Ссылка на JSON')
+
+    def handle(self, *args, **options):
+        json_url = options['json_url']
+        response = requests.get(json_url)
+        response.raise_for_status()
+        data = response.json()
+
+        place, created = Place.objects.get_or_create(
+            title=data['title'],
+            defaults={
+                'description_short': data.get('description_short', ''),
+                'description_long': data.get('description_long', ''),
+                'latitude': data['coordinates']['lat'],
+                'longitude': data['coordinates']['lng'],
+            }
+        )
+
+        if not created:
+            self.stdout.write(self.style.WARNING(f'Место "{place.title}" уже есть'))
+        else:
+            self.stdout.write(self.style.SUCCESS(f'Создано место: {place.title}'))
+
+        # Картинки
+        for idx, img_url in enumerate(data.get('imgs', []), start=1):
+            img_response = requests.get(img_url)
+            img_response.raise_for_status()
+            img_name = img_url.split('/')[-1]
+            image = PlaceImage(place=place, order=idx)
+            image.image.save(img_name, ContentFile(img_response.content), save=True)
+            self.stdout.write(self.style.SUCCESS(f'Загружено изображение: {img_name}'))
+
+        self.stdout.write(self.style.SUCCESS(f'Место "{place.title}" загружено полностью!'))
