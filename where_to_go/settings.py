a1bc7d348e04bcd35af374dbe4c310c77ac90ef4
MaksimObozniy <mobozn@gmail.com>
2025-06-05 17:49:28 +0400

    Move settings to environment variables and prepare for production deployment

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG']
 
 ALLOWED_HOSTS = []
-
+ 
 
 # Application definition
 
