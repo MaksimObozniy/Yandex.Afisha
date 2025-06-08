a40de0a201cb3a32dc2f7f62557c789b6b4e8636
MaksimObozniy <mobozn@gmail.com>
2025-06-08 13:20:42 +0400

    Update verbose_name for place title

--- places/models.py
+++ places/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from tinymce.models import HTMLField
 
 class Place(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='Название места')
     description_short = models.TextField(verbose_name='Краткое описание')
     description_long = HTMLField(verbose_name='Длинное описание')
     latitude = models.FloatField(verbose_name='Широта')
